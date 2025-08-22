29741fa8df41137c6ffee00a42d28119d7997481
Rocky-hc <3134947642@qq.com>
2025-08-23 00:26:49 +0800

    done with lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -77,6 +77,7 @@ public class IntListExercises {
             lst.first *= lst.first;
         }
 
-        return  squarePrimes(lst.rest) || currElemIsPrime ;
+        //return  squarePrimes(lst.rest) || currElemIsPrime ;
+        return currElemIsPrime || squarePrimes(lst.rest);
     }
 }
